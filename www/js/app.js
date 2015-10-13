fbfb67ef1bcfd913c6956d0ec95cda380dffb281
Marc-André Langlais <tropic_cancer@hotmail.com>
2015-10-13 09:23:57 +0000

    trying new data format

--- www/js/app.js
+++ www/js/app.js
@@ -143,6 +143,6 @@ function getArray(htmlStr) {
 function getVIEWSTATE(htmlstr) {
   var index;
   var wholeTxt = htmlstr.split('<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="');
-  var halfTxt = wholeTxt.split('</div>');
+  var halfTxt = wholeTxt[1].split('</div>');
   return halfTxt;
 }
