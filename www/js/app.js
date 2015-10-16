14d6385020e2cccd19ad593a33c664ed3df79275
Marc-André Langlais <tropic_cancer@hotmail.com>
2015-10-16 01:52:40 +0000

    going fwd

--- www/js/app.js
+++ www/js/app.js
@@ -41,9 +41,7 @@ angular.module('open_schedule', ['ionic'])
 })
 
 .controller('teamCtrl', function($scope, $http, $window) {
-  
-  var dataDetails = { 'm$pc$cbYear':'2015', 'm$pc$cbCategories':'4', 'm$pc$cbSemaine':'2015-10-12', 'm$pc$cbArenaFilter': '-1'};  
-  sendData(dataDetails);
+  sendData();
 })
 
 .run(function($ionicPlatform) {
@@ -127,38 +125,37 @@ function getVIEWSTATE(htmlstr) {
   return result;
 }
 
-function sendData(data) {
-  var XHR = new XMLHttpRequest();
-  var urlEncodedData = "";
-  var urlEncodedDataPairs = [];
-  var name;
+function sendData() {
+  var xml = new XMLHttpRequest();
+  var args      = { url:"http://lcrse.qc.ca/cedules.saison.aspx",method:"POST",data:{ '__EVENTTARGET':'m$pc$cbCategories', '__EVENTARGUMENT':'', 'm$pc$cbYear':'2015', 'm$pc$cbCategories':'4', 'm$pc$cbSemaine':'2015-10-12', 'm$pc$cbArenaFilter': '-1'}, callback:function(){}}
+  var multipart ="";
+  var context;
 
-  // We turn the data object into an array of URL encoded key value pairs.
-  for(name in data) {
-    urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
-  }
+  xml.open(args.method,args.url,true);
 
-  // We combine the pairs into a single string and replace all encoded spaces to 
-  // the plus character to match the behaviour of the web browser form submit.
-  urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+    if(args.method.search(/post/i)!=-1){
+      var boundary=Math.random().toString().substr(2);
+      xml.setRequestHeader("content-type",
+                  "multipart/form-data; charset=utf-8; boundary=" + boundary);
+      for(var key in args.data){
+        multipart += "--" + boundary
+                   + "\r\nContent-Disposition: form-data; name=" + key
+                   + "\r\nContent-type: application/octet-stream"
+                   + "\r\n\r\n" + args.data[key] + "\r\n";
+      }
+      multipart += "--"+boundary+"--\r\n";
+    }
 
-  // We define what will happen if the data is successfully sent
-  XHR.addEventListener('load', function(event) {
-    alert('Yeah! Data sent and response loaded.');
-  });
+    xml.onreadystatechange=function(){
+      try{
+        if(xml.readyState==4){
+          context.txt=xml.responseText;
+          context.xml=xml.responseXML;
+          args.callback();
+        }
+      }
+      catch(e){}
+    }
 
-  // We define what will happen in case of error
-  XHR.addEventListener('error', function(event) {
-    alert('Oups! Something goes wrong.');
-  });
-
-  // We setup our request
-  XHR.open('POST', 'http://lcrse.qc.ca/cedules.saison.aspx');
-
-  // We add the required HTTP header to handle a form data POST request
-  XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  XHR.setRequestHeader('Content-Length', urlEncodedData.length);
-
-  // And finally, We send our data.
-  XHR.send(urlEncodedData);
+    xml.send(multipart);
 }
